5e25c505ecff6471d51e4f254e741d9140d2c3a6
Salvador Guzman <guzmansalv@gmail.com>
2020-12-20 11:20:57 -0800

    added schema

--- schema/items.sql
+++ schema/items.sql
@@ -1,4 +1,56 @@
 create table items
 (
+	id int not null,
+	name text not null,
+	incomplete bool not null,
+	members bool not null,
+	tradeable bool not null,
+	tradeable_on_ge bool not null,
+	stackable bool not null,
+	stacked bool,
+	noted bool not null,
+	noteable bool not null,
+	linked_id_item int not null,
+	linked_id_noted int,
+	linked_id_placeholder int,
+	equipable bool not null,
+	equipable_by_player bool not null,
+	equipable_weapon bool not null,
+	cost int not null,
+	lowalch int,
+	highalch int,
+	weight float not null,
+	buy_limit int,
+	quest_item bool not null,
+	release_date date,
+	duplicate bool not null,
+	examine text not null,
+	icon bytea,
+	wiki_name text not null,
+	wiki_url text not null,
+	attack_stab int,
+    attack_slash int,
+    attack_crush int,
+    attack_magic int,
+    attack_ranged int,
+    defence_stab int,
+    defence_slash int,
+    defence_crush int,
+    defence_magic int,
+    defence_ranged int,
+    melee_strength int,
+    ranged_strength int,
+    magic_damage int,
+    prayer int,
+    slot text,
+    attack_speed int,
+    weapon_type text
 );
+
+create unique index items_id_uindex
+	on items (id);
+
+alter table items
+	add constraint items_pk
+		primary key (id);
 
